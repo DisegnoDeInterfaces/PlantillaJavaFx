da65d1a4840f6047f3c9f89ff020f790ded5f91a
carlosfernandez <prof.carlos.tessier@gmail.com>
2018-03-29 13:08:04 +0200

    Separando Layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,18 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group root = new Group();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        /*Group root = new Group();
 
         Text txt = new Text("JavaFx");
         txt.setFont(new Font("Papyrus",80));
@@ -38,7 +33,7 @@ public class Main extends Application {
 
         VBox box = new VBox();
         box.getChildren().addAll(txt, grid);
-        root.getChildren().add(box);
+        root.getChildren().add(box);*/
 
         primaryStage.setTitle("Plantilla JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
