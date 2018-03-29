b9f630bfdae22d65a37dabc3ce1a049c15c64bc0
carlosfernandez <prof.carlos.tessier@gmail.com>
2018-03-29 15:04:18 +0200

    El Controlador

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField nombre;
+
+    public void accionSaludar(ActionEvent actionEvent) {
+        System.out.printf("¡Hola %s! %n",nombre.getText());
+    }
 }
