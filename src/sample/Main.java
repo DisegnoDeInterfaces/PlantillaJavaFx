59e5b9df38968a29be3398fa53cbd99011aaf3d7
carlosfernandez <prof.carlos.tessier@gmail.com>
2018-03-29 11:52:42 +0200

    Creando nodos

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,21 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+    public void start(Stage primaryStage) {
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Group root = new Group();
+        Text txt = new Text("Hola mundo");
+        root.getChildren().add(txt);
+        txt.setY(50);
+        primaryStage.setTitle("Plantilla JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
