138573ca518f1300cf25004816b23a3006285062
carlosfernandez <prof.carlos.tessier@gmail.com>
2018-03-29 12:05:34 +0200

    Añadiendo interactividad

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,9 +15,18 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Group root = new Group();
-        Text txt = new Text("Hola mundo");
-        root.getChildren().add(txt);
-        txt.setY(50);
+
+        Text txt = new Text("¿Quién eres?");
+        TextField nameFld = new TextField();
+
+        Button btn = new Button();
+        btn.setText("Hola");
+        btn.setOnAction(event -> System.out.printf("¡Hola %s! %n",nameFld.getText()));
+
+        VBox box = new VBox();
+        box.getChildren().addAll(txt, nameFld, btn);
+        root.getChildren().add(box);
+
         primaryStage.setTitle("Plantilla JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
